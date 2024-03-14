fb24de1cadd29400dc2ed5e4dc41c8f23e9a2b04
Michael Avagianos <mixalisava@gmail.com>
2024-03-14 13:30:07 +0200

    feat: use arr.swap( ) instead of temp variable

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ fn bubble_sort(mut arr: Vec<i32>) -> Vec<i32> {
     for i in 0..(arr.len()) {
         for j in 0..(arr.len() - i - 1) {
             if arr[j] > arr[j + 1] {
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                arr.swap(j, j + 1);
             }
         }
     }
@@ -21,7 +19,7 @@ mod tests {
     fn bubble_test() {
         let arr = vec![5, 3, 8, 4, 2, 6, 9, 1, 7];
         let result = bubble_sort(arr);
-        assert_eq!(result, vec![1, 2, 3, 4, 5, 6, 7, 8, 9])
+        assert_eq!(result, vec![1, 2, 3, 4, 5, 6, 7, 8, 9]);
     }
 }
 
