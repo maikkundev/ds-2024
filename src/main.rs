59c1da412c9d20ab9c6d58be8c7e82b35b865dbc
Michael Avagianos <mixalisava@gmail.com>
2024-03-16 21:08:24 +0200

    chore: improved test and code comments

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 fn merge(arr: &mut Vec<i32>, start: usize, mid: usize, end: usize) {
-    // create subarrays
+    // Create subarrays
     let left = arr[start..=mid].to_vec();
     let right = arr[mid + 1..=end].to_vec();
 
-    // index of subarrays and main array
+    // Index of subarrays and main array
     let mut i = 0;
     let mut j = 0;
     let mut k = start;
 
-
-    // pick larger among
-    // elements L and M and place them in the correct position
+    // Pick larger among elements Vec left and right and place them in the correct position
     while i < left.len() && j < right.len() {
         if left[i] <= right[j] {
             arr[k] = left[i];
@@ -22,6 +20,7 @@ fn merge(arr: &mut Vec<i32>, start: usize, mid: usize, end: usize) {
         k += 1;
     }
 
+    // Pick up the remaining elements when either Vec left or right run out of elements
     while i < left.len() {
         arr[k] = left[i];
         i += 1;
@@ -37,26 +36,32 @@ fn merge(arr: &mut Vec<i32>, start: usize, mid: usize, end: usize) {
 
 fn merge_sort(arr: &mut Vec<i32>, start: usize, end: usize) {
     if start < end {
-        let m = start + (end - start) / 2;
+        let middle = start + (end - start) / 2;
 
-        merge_sort(arr, start, m);
-        merge_sort(arr, m + 1, end);
+        merge_sort(arr, start, middle);
+        merge_sort(arr, middle + 1, end);
 
-        merge(arr, start, m, end);
+        // Merge sorted subarrays
+        merge(arr, start, middle, end);
     }
 }
 
+// CMD: Cargo test
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn bubble_test() {
+    fn merge_sort_test() {
         let mut arr = vec![5, 3, 8, 4, 2, 6, 9, 1, 7];
         let arr_len = arr.len();
         let mut sorted_arr = arr.clone();
         sorted_arr.sort();
         merge_sort(&mut arr, 0, arr_len - 1);
+        print!(
+            "Array: {:?}, Sorted Array with Merge Sort: {:?}",
+            arr, sorted_arr
+        );
         assert_eq!(arr, sorted_arr)
     }
 }
