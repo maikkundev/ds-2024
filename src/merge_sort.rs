214655288da9ecf2fe0a43ba1393f5fd7f4b8fc1
Michael Avagianos <mixalisava@gmail.com>
2024-03-17 18:22:37 +0200

    feat: made function a module

--- src/merge_sort.rs
+++ src/merge_sort.rs
@@ -34,7 +34,7 @@ fn merge(arr: &mut Vec<i32>, start: usize, mid: usize, end: usize) {
     }
 }
 
-fn merge_sort(arr: &mut Vec<i32>, start: usize, end: usize) {
+pub fn merge_sort(arr: &mut Vec<i32>, start: usize, end: usize) {
     if start < end {
         let middle = start + (end - start) / 2;
 
@@ -45,25 +45,3 @@ fn merge_sort(arr: &mut Vec<i32>, start: usize, end: usize) {
         merge(arr, start, middle, end);
     }
 }
-
-// CMD: Cargo test
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn merge_sort_test() {
-        let mut arr = vec![5, 3, 8, 4, 2, 6, 9, 1, 7];
-        let arr_len = arr.len();
-        let mut sorted_arr = arr.clone();
-        sorted_arr.sort();
-        merge_sort(&mut arr, 0, arr_len - 1);
-        print!(
-            "Array: {:?}, Sorted Array with Merge Sort: {:?}",
-            arr, sorted_arr
-        );
-        assert_eq!(arr, sorted_arr)
-    }
-}
-
-fn main() {}
